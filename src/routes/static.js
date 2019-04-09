3d26c2e215ad074db55bfacc8ac05cebe6c4326d
James Smith <james@Jamess-MacBook-Air.local>
2019-04-09 13:56:17 -0600

    updated routing checkpoint

--- src/routes/static.js
+++ src/routes/static.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const staticController = require("../controllers/staticCOntroller");
+const staticController = require("../controllers/staticController");
 
 router.get("/", staticController.index);
 
